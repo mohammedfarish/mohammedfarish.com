21105ba925012d65ec0337a0083b3c54905b2a14
Mohammed Farish <farishmusthafa@gmail.com>
2021-05-09 19:28:32 +0400

    Minor fix

--- pages/api/blog/upload.js
+++ pages/api/blog/upload.js
@@ -1,35 +1,34 @@
 import cloudinary from 'cloudinary'
 
+// THIS IS A TEST ROUTE
+
 export default async (req, res) => {
+    const { method } = req
 
+    switch (method) {
+        case 'POST':
+            try {
 
-    try {
-        cloudinary.v2.config({
-            cloud_name: 'farish',
-            api_key: '622758927776665',
-            api_secret: '1R0uaP9hYPPkEvgK2hAzTz8sFjI'
-        })
-        // console.log(req.body);
-        // const image = await cloudinary.v2.image(req.body)
+                cloudinary.v2.config({
+                    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+                    api_key: process.env.CLOUDINARY_API_KEY,
+                    api_secret: process.env.CLOUDINARY_API_SECRET
+                })
 
-        // Buffer
+                if (req.file) return console.log(req.file)
 
-        // await multerUploads(req, res)
+                res.json({ success: false, url: "This is a test route." });
 
-        if (req.file) return console.log(req.file)
+            } catch (error) {
+                console.log(error);
+                res.json(false)
+            }
 
-        // const file = await fs.readFile()
+            break;
 
-        // const data = await cloudinary.v2.uploader.upload(file)
-        // const data = await cloudinary.v2.uploader.upload('https://cdn.discordapp.com/attachments/726328135393476609/832274177247150130/Logo_1.png')
-        // const url = await data.url
-
-        // res.json({ success: true, url })
-        console.log(url)
-
-    } catch (error) {
-        console.log(error);
-        res.json(false)
+        default:
+            res.status(404).json(false)
+            break;
     }
 
 }
